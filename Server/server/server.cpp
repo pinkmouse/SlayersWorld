0de17530cc942aae64bd497c67b43abe8a87c953
Lynix <lynix680@gmail.com>
2016-09-25 02:44:36 +0200

    Server: Fix network initialization

--- Server/server/server.cpp
+++ Server/server/server.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "Map/Map.hpp"
 #include "World/World.hpp"
+#include <Nazara/Network/Network.hpp>
 #include <NDK/Application.hpp>
 
 ConfigHandler *g_Config;
@@ -8,6 +9,7 @@ ConfigHandler *g_Config;
 int main(int argc, char* argv[])
 {
 	Ndk::Application app(argc, argv);
+	Nz::Initializer<Nz::Network> network;
 
 	printf("Starting Slayers World...\n");
 	World* l_World = new World();
