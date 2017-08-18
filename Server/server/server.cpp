9e5b8289aa3a3aa578af15d90c9d2b50486d0bc6
Justin Mariette <justin.mariette@gmail.com>
2017-08-18 21:35:29 +0200

    Fix compil

--- Server/server/server.cpp
+++ Server/server/server.cpp
@@ -34,7 +34,7 @@ void handler_segfault(int sig) {
     std::string l_FinalStr = "";
     for (uint8 i = 0; i < 20; i++)
     {
-        char* l_Str = static_cast<char*>l_Array[i];
+        char* l_Str = static_cast<char*>(l_Array[i]);
         l_FinalStr += std::string(l_Str) + " ";
     }
     WebHook::sendMsg(g_Config->GetValue("WebhookUrl"), l_FinalStr);
