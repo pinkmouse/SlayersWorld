f37ab8cf4ecdf2d8b7532bcfc1d03bf46dcc0605
Justin Mariette <justin.mariette@gmail.com>
2017-08-18 17:50:42 +0200

    Remove useless hook

--- Server/server/System/WebHook.hpp
+++ Server/server/System/WebHook.hpp
@@ -17,11 +17,11 @@ public:
             list = curl_slist_append(list, "Content-Type: application/json");
 
             curl_easy_setopt(l_Curl, CURLOPT_URL, p_URL.c_str());
-            curl_easy_setopt(l_Curl, CURLOPT_POST, 1);
+            /*curl_easy_setopt(l_Curl, CURLOPT_POST, 1);
             curl_easy_setopt(l_Curl, CURLOPT_HTTPHEADER, list);
             curl_easy_setopt(l_Curl, CURLOPT_SSL_VERIFYHOST, false);
             curl_easy_setopt(l_Curl, CURLOPT_SSL_VERIFYPEER, false);
-            curl_easy_setopt(l_Curl, CURLOPT_POSTFIELDSIZE, p_Msg.size());
+            curl_easy_setopt(l_Curl, CURLOPT_POSTFIELDSIZE, p_Msg.size());*/
             curl_easy_setopt(l_Curl, CURLOPT_POSTFIELDS, p_Msg.c_str());
             l_Result = curl_easy_perform(l_Curl);
             curl_easy_cleanup(l_Curl);
