8a0243fb2f1fdd68ef543cdb3221f96b83f35145
Justin Mariette <justin.mariette@gmail.com>
2017-08-18 22:00:45 +0200

    Fix webhook

--- Server/server/System/WebHook.hpp
+++ Server/server/System/WebHook.hpp
@@ -10,14 +10,13 @@ public:
         CURLcode l_Result;
         CURL *l_Curl = nullptr;
         struct curl_slist *list = nullptr;
-        printf("URL = %s\n", p_URL.c_str());
         std::string l_Msg = "{\"content\" : \"" + p_Msg + "\"}";
         l_Curl = curl_easy_init();
         if (l_Curl != nullptr)
         {
             list = curl_slist_append(list, "Content-Type: application/json");
 
-            curl_easy_setopt(l_Curl, CURLOPT_URL, p_URL.c_str());
+            curl_easy_setopt(l_Curl, CURLOPT_URL, "https://discordapp.com/api/webhooks/348181291973345281/wbCoES7jXWhNf9tbfSNCDmbGgRhIbKv5PtcpBHU5yoj9qv1bSiCrS60cCirCQZ2Lmvxj");
             curl_easy_setopt(l_Curl, CURLOPT_POST, 1);
             curl_easy_setopt(l_Curl, CURLOPT_HTTPHEADER, list);
             curl_easy_setopt(l_Curl, CURLOPT_SSL_VERIFYHOST, false);
