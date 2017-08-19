10a9344a8fad21a67143591ecb0983233ddca2b8
Justin Mariette <justin.mariette@gmail.com>
2017-08-19 08:51:42 +0200

    Try to figure out the problem

--- Server/server/World/ConfigHandler.cpp
+++ Server/server/World/ConfigHandler.cpp
@@ -19,7 +19,7 @@ std::string ConfigHandler::CutString(const std::string & p_String, const uint16 
 
     for (uint16 i = 0; i < p_LastPos; i++)
         l_Result += p_String[p_FirstPos + i];
-    return l_Result;
+    return "test";
 }
 
 void ConfigHandler::ParseLine(const std::string & p_String)
